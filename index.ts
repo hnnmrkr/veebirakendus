df9fe15f470d61e64f017d670afc3aae333e9a77
Hanna <hanna-marii.kriisa@voco.ee>
2023-03-24 16:49:32 +0200

    Omniva, Elering, Payment

--- index.ts
+++ index.ts
@@ -1,9 +1,13 @@
 import express, { Express, Request, Response } from "express";
 import cors from "cors";
+import axios from "axios";
 import bodyParser from "body-parser";
 import stringsController from "./controllers/strings";
 import productsController from "./controllers/products";
 import productListController from "./controllers/productlist";
+import parcelmachines from "./controllers/parcelmachines";
+import nordPoolPrice from "./controllers/nord-pool-price";
+import payment from "./controllers/payment";
 
 const app: Express = express();
 
@@ -16,10 +20,13 @@ app.get('/', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server');
 });
 
+app.use('/', payment);
 app.use('/', stringsController);
 app.use('/', productsController);
 app.use('/', productListController);
+app.use('/', parcelmachines);
+app.use('/', nordPoolPrice);
 
 app.listen(3001,() => {
-    console.log(`[server]: Server is running at http://localhost:3000`);
+    console.log(`[server]: Server is running at http://localhost:3001`);
 });
